f1816fb3855cdd46cfe8618dc7263f80964db60e
Sergey Komarov <anaxronik@gmail.com>
2021-05-24 03:00:01 +0500

    create color vars and download fonts

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,10 @@ module.exports = {
     ],
   },
   devServer: {
-    contentBase: './dist',
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+    // host: '0.0.0.0',
+    open: true,
   },
 }
