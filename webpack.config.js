b6b1a239975f9d7ac470a1f3d97a66eb8cf631bb
Sergey Komarov <anaxronik@gmail.com>
2021-05-24 01:34:05 +0500

    create webpack pug settings

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/pug/index.pug',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader',
+        options: {
+          pretty: true,
+        },
+      },
+    ],
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+}
