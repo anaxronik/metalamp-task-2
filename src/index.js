3dd72b5ad4d73c95fae401da5c246fd41b0199f1
Sergey Komarov <anaxronik@gmail.com>
2021-05-24 02:16:01 +0500

    connect scss

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
+import './styles/style.scss'
+// import './styles/style.css'
+
 console.log('index js run')
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,25 @@
 const path = require('path')
-var HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   entry: './src/index.js',
   mode: 'development',
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'docs'),
+    clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: './src/pug/index.pug',
+      template: './src/pug/pages/index.pug',
     }),
+    new HtmlWebpackPlugin({
+      filename: 'uikit.html',
+      template: './src/pug/pages/uikit.pug',
+    }),
+    new MiniCssExtractPlugin(),
   ],
   module: {
     rules: [
@@ -22,6 +29,14 @@ module.exports = {
         options: {
           pretty: true,
         },
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
     ],
   },
