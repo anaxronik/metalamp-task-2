82a128d585e22abb27958c5c8a66bc812665f846
Sergey Komarov <anaxronik@gmail.com>
2021-05-24 15:05:30 +0500

    добавленна поддержка нескольких страниц для pug

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,17 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const fs = require('fs')
+
+const PATHS = {
+  src: path.join(__dirname, './src'),
+  dist: path.join(__dirname, './docs'),
+  assets: 'assets/',
+}
+const PAGES_DIR = `${PATHS.src}/pug/pages`
+const PAGES = fs
+  .readdirSync(PAGES_DIR)
+  .filter((fileName) => fileName.endsWith('.pug'))
 
 module.exports = {
   entry: './src/index.js',
@@ -11,15 +22,19 @@ module.exports = {
     clean: true,
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: './src/pug/pages/index.pug',
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'uikit.html',
-      template: './src/pug/pages/uikit.pug',
-    }),
     new MiniCssExtractPlugin(),
+    ...PAGES.map(
+      (page) =>
+        new HtmlWebpackPlugin({
+          template: `${PAGES_DIR}/${page}`,
+          filename: `./${page.replace(/\.pug/, '.html')}`,
+          minify: {
+            collapseWhitespace: true,
+          },
+          title: 'Webpack + Pug template',
+          inject: 'body',
+        })
+    ),
   ],
   module: {
     rules: [
